8e8e8648d276d1eeac8bbddc6732e5703e7ec406
Indgie <124298620+Indgie@users.noreply.github.com>
2023-10-11 14:49:15 +0300

    create page structure and form elements

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/*let alertMessage = document.querySelector(".alert");
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", () => {
+    let firstPassword = document.querySelector("#up");
+    let secondPassword = document.querySelector("#upr");
+
+    
+    if (firstPassword.value !== secondPassword.value) {
+        alertMessage.textContent = "password not matching";
+    }
+    else {
+        alertMessage.textContent = "registration complete";
+    }
+
+})*/
+
